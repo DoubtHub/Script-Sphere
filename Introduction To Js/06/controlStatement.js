f4d9f3a588c22ed302a23d7853038e243cf4539b
akhtab-fahim <md.fahimakhtab04@gmail.com>
2025-03-03 13:08:53 +0530

    Refactor control statements and add loops example

--- Introduction To Js/06/controlStatement.js
+++ Introduction To Js/06/controlStatement.js
@@ -26,26 +26,6 @@ if(userLoggedIn){
     
 }
 
-//while
-
-let z = 1
-
-while(z<=10){
-    console.log(z)
-    z+=2
-}
-
-let sum = 10
-
-//do-while
-
-do {
-    console.log(sum);
-    sum++;
-    
-} while (z>20);
-
-
 //switch
 
 let day = "Monday";
---
+++ Introduction To Js/07/loops.js
@@ -0,0 +1,18 @@
+//while
+
+let z = 1
+
+while(z<=10){
+    console.log(z)
+    z+=2
+}
+
+let sum = 10
+
+//do-while
+
+do {
+    console.log(sum);
+    sum++;
+    
+} while (z>20);
